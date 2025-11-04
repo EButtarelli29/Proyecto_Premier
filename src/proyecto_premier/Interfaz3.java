b5358ecbdd8f5e769bd4c70b30580cf45927698c
EButtarelli29 <arkiam72@gmail.com>
2025-11-04 15:47:35 -0300

    Nuevos archivos

--- src/proyecto_premier/Interfaz3.java
+++ src/proyecto_premier/Interfaz3.java
@@ -174,11 +174,15 @@ public class Interfaz3 extends javax.swing.JFrame {
     }//GEN-LAST:event_userBtnActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
+        this.setVisible(false);
+        IngRegistros ingregistros = new IngRegistros();
+        ingregistros.setVisible(true);
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
+        this.setVisible(false);
+        RevRegistros revregistros = new RevRegistros();
+        revregistros.setVisible(true);
     }//GEN-LAST:event_jButton2ActionPerformed
 
     
